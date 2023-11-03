0adce70e01427943e252640057dec797ed85820a
Vidit Aggarwal <vidit.aggarwal@gmail.com>
2023-11-02 22:05:31 -0700

    Reading cookie

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cookies from "js-cookie";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss?v=1.3.0";
@@ -11,10 +12,20 @@ import AdminLayout from "layouts/Admin.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<AdminLayout />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  useEffect(() => {
+    const smSession = Cookies.get('smSession');
+    console.log('smSession cookie:', smSession);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AdminLayout />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+root.render(<App />);
+
